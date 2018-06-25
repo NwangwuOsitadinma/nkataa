ddf2954a16af39037b9f45e761f2ceec10d35f91
Ibesoft11 <ibesoft11@gmail.com>
2018-06-25 13:51:21 +0100

    Added enpoints for viewing and deleting user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,8 +6,21 @@ exports.addUser = function(req, res){
         email: req.body.email,
         password: req.body.password
     };
-    model.create(data, function(err, User){
+    model.create(data, function(err){
         if (err) res.json({err: err, message: 'the user could not be created'});
         res.json({message: 'user created successfully'});
+    });
+}
+exports.getUsers = function (req, res){
+    model.find(function(err, users){
+        if (err) res.json({err: err, message: 'something went wrong'});
+        res.json(users);
+    });
+}
+exports.deleteUser = function(req, res){
+    var options = {_id: req.params.id};
+    model.remove(options, function(err){
+        if (err) res.json({err:err, message:'an error occurred while deleting'});
+        res.json({message: 'user deleted'});
     });
 }
