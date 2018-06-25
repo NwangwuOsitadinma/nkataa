c933837685b936951ee84620d6932e5238005a4c
Ibesoft11 <ibesoft11@gmail.com>
2018-06-25 12:12:41 +0100

    Updated user model

--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 var UserSchema = mongoose.Schema({
    name: String,
-   email: String,
-   
+   email: { type: String, unique: true},
+   password: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
