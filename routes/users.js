24c38715d9806f4348cdf1e5686f99828cbce35a
Ibesoft11 <ibesoft11@gmail.com>
2018-06-25 13:52:31 +0100

    Added routers for returning and delete user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+var userController = require('../controllers/UserController');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.post('/create', userController.addUser);
+router.get('/', userController.getUsers);
+router.get('/delete/:id', userController.deleteUser);
 module.exports = router;
