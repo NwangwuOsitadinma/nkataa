6d34cea05dce025257ebb993faa52d1b35bed987
Ibesoft11 <ibesoft11@gmail.com>
2018-06-26 14:41:30 +0100

    Added routes for posts

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 
@@ -25,6 +26,7 @@ mongoose.connect('mongodb://localhost:27017/nkataa');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
