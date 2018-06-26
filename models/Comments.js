1b8a4f39251ed39700406e5d6985afbabdb8d019
Ibesoft11 <ibesoft11@gmail.com>
2018-06-26 15:18:30 +0100

    Modified schema for comments

--- models/Comments.js
+++ models/Comments.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 
 var commentSchema = mongoose.Schema({
     time: Date,
-    commentBody: String
+    commentBody: String,
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'}
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
