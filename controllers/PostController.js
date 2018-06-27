290b769795a7b8eef8b9d5099fad0e7da84f3f06
Ibesoft11 <ibesoft11@gmail.com>
2018-06-27 08:50:37 +0100

    Modified error messages for every db operation

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -8,15 +8,14 @@ exports.createPost = function(req, res){
         comments: [],
     }
     model.create(data, function(err){
-        if (err) res.json({err: err, message: 'the post could not be created'});
+        if (err) res.json({err: err, message: 'Sorry, the post could not be created'});
         res.json({message: 'post created successfully'});
     });
 }
 
 exports.viewPosts = function(req, res){
-    //var user = req.body.user;
     model.find(function(err, posts){
-        if (err) res.json({err: err, message: 'something went wrong'});
+        if (err) res.json({err: err, message: 'something went wrong while fetching posts'});
         res.json(posts);
     });
 }
@@ -24,7 +23,7 @@ exports.viewPosts = function(req, res){
 exports.deletePost = function(req, res){
     var options = {_id: req.params.id};
     model.remove(options, function(err){
-        if (err) res.json({err:err, message:'an error occurred while deleting'});
+        if (err) res.json({err:err, message:'an error occurred while deleting the post'});
         res.json({message: 'post deleted'});
     });
 }
@@ -32,10 +31,10 @@ exports.deletePost = function(req, res){
 exports.editPost = function(req, res){
     var id = req.body.id;
     model.findById(id, function(err, post){
-        if (err) res.json({err:err, message:'sorry, an error occurred'});
+        if (err) res.json({err:err, message:'sorry, an error occurred while editing the post'});
         if (req.body.postBody) post.postBody = req.body.postBody;
         post.save(function (err, updatedPost){
-            if (err) res.json({err:err, message:'sorry an error occurred'});
+            if (err) res.json({err:err, message:'sorry an error occurred while editing the post'});
             res.json(updatedPost);
         });
     });
@@ -44,7 +43,7 @@ exports.editPost = function(req, res){
 exports.viewPostsByUser = function(req, res){
     var user = req.params.userId;
     model.find({user:user}, 'postBody', function(err, data){
-        if (err) res.json({err:err, message:'sorry, something went wrong while searching'});
+        if (err) res.json({err:err, message:'sorry, something went wrong while retrieving user posts'});
         res.json({message: data});
     });
 }
